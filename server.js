18e4b2745a3c11cf4a7957d0ec983e89efa3eaa8
Alexander Schenkel <aschenkel@kadenpartner.ch>
2019-03-16 17:39:32 +0100

    Documentation baseline

---
+++ server.js
@@ -0,0 +1,22 @@
+const server = require('pcms');
+const path = require('path');
+
+// Read the site's config file: This is needed by the server.
+const debug = require('debug')('server');
+
+/**
+ * Start the server, which is an expressjs app. You can get the express app instance if you
+ * need to configure things in advance:
+ *
+ * const app = server.expressApp;
+ */
+server
+    // give the full path to your site-config.json: The server needs it to gather information
+    // about your directory origin:
+    .start(path.join(__dirname, 'site-config.json'))
+    .then(app => {
+        debug(`Site is now serving at port: ${app.serverConfig.siteConfig.port}`);
+    })
+    .catch(err => {
+        debug('ERROR: ', err);
+    });
